71ead86f65035b9bbc6c913adc3b0268bf644b23
Unknown <unknown@example.com>
2020-07-02 19:42:17 -0500

    Primera propuesta de estandar

--- src/main/java/com/clean/swing/app/dashboard/DashboardExtendedDown.java
+++ src/main/java/com/clean/swing/app/dashboard/DashboardExtendedDown.java
@@ -1,10 +1,14 @@
 package com.clean.swing.app.dashboard;
 
+import javax.swing.Action;
+
 /**
  *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
-public interface DashboardExtendedDown<MainType, DownType> extends DashboardSimple<MainType> {
+public interface DashboardExtendedDown<ElementType> {
 
-    public void addDownElement(DownType element);
+    public void addDownElement(ElementType element);
+
+    public void setLicence(Action licence);
 }
--- src/main/java/com/clean/swing/app/dashboard/DashboardSimple.java
+++ src/main/java/com/clean/swing/app/dashboard/DashboardSimple.java
@@ -1,10 +1,17 @@
 package com.clean.swing.app.dashboard;
 
+import java.awt.Component;
+import java.awt.Container;
+
 /**
  *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
-public interface DashboardSimple<MainType> {
+public abstract class DashboardSimple<MainType> extends Container {
 
-    public void addMainElement(MainType element);
+    public abstract void addMainElement(MainType element);
+
+    public abstract void addView(String name, Component compoment);
+
+    public abstract void showView(String name);
 }
