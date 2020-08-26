99f6e980551e72bb6e0424856502031a2a745da0
Unknown <unknown@example.com>
2020-08-25 22:19:05 -0500

    +DefaultAbstractSwingMainModule

---
+++ src/main/java/com/clean/swing/app/DefaultAbstractSwingMainModule.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clean.swing.app;
+
+public abstract class DefaultAbstractSwingMainModule implements AbstractSwingMainModule {
+
+    @Override
+    public void register(AbstractSwingApplication dashBoard) {
+    }
+
+    @Override
+    public void closeModule() {
+    }
+
+    @Override
+    public void navigateTo(String string, Object... os) {
+    }
+
+}
