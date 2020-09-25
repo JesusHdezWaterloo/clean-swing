5a2eaf46e7da2510b45a00207a41d70b9317b888
Unknown <unknown@example.com>
2020-09-25 01:38:46 -0500

    +DashBoardComponent

---
+++ src/main/java/com/clean/swing/utils/DashBoardComponent.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clean.swing.utils;
+
+import java.awt.Component;
+import javax.swing.Icon;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public class DashBoardComponent {
+
+    public String name;
+    public Icon icon;
+    public String nav;
+    public Component view;
+
+    public DashBoardComponent(String name, Icon icon, String nav, Component view) {
+        this.name = name;
+        this.icon = icon;
+        this.nav = nav;
+        this.view = view;
+    }
+
+}
