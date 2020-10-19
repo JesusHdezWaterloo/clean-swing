22a7aa5374cd7a9e638885ab14234ab9ad797c28
Unknown <unknown@example.com>
2020-10-19 16:25:15 -0500

    DashBoardComponent: +tooltip, +buildAction

--- src/main/java/com/clean/swing/utils/DashBoardComponent.java
+++ src/main/java/com/clean/swing/utils/DashBoardComponent.java
@@ -5,7 +5,11 @@
  */
 package com.clean.swing.utils;
 
+import com.clean.core.app.services.NavigationService;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.Icon;
 
 /**
@@ -18,16 +22,36 @@ public class DashBoardComponent {
         return new DashBoardComponent(name, icon, nav, view);
     }
 
+    public static DashBoardComponent from(String name, String toolTip, Icon icon, String nav, Component view) {
+        return new DashBoardComponent(name, toolTip, icon, nav, view);
+    }
+
     public String name;
+    public String toolTip;
     public Icon icon;
     public String nav;
     public Component view;
 
     public DashBoardComponent(String name, Icon icon, String nav, Component view) {
+        this(name, name, icon, nav, view);
+    }
+
+    public DashBoardComponent(String name, String toolTip, Icon icon, String nav, Component view) {
         this.name = name;
+        this.toolTip = toolTip;
         this.icon = icon;
         this.nav = nav;
         this.view = view;
     }
 
+    public AbstractAction buildAction(NavigationService navigator) {
+        AbstractAction act = new AbstractAction(name, icon) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                navigator.navigateTo(nav);
+            }
+        };
+        act.putValue(Action.SHORT_DESCRIPTION, toolTip);
+        return act;
+    }
 }
